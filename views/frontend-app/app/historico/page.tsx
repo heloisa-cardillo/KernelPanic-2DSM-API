3a28a3efa72a3c83ae0cbe6cd2d5b589dabab465
VitorSerpa <vitorserpa123@gmail.com>
2025-09-08 21:23:24 -0300

    feat(Rotas criadas)

--- views/frontend-app/app/historico/page.tsx
+++ views/frontend-app/app/historico/page.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Historico() {
   return (
     <div>
-        
+        <h1>Histórico</h1>
     </div>
   )
 }
--- views/frontend-app/app/layout.tsx
+++ views/frontend-app/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import LateralBar from "@/components/layout/LateralBar";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +29,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <LateralBar></LateralBar>
         {children}
       </body>
     </html>
