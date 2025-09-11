8f4912b9962d4f6d130d636196e812ed19d505ca
Vitor Serpa <vitorserpa123@gmail.com>
2025-09-11 12:23:37 -0300

    fix(Arrumando importações e conflitos)

--- views/frontend-app/app/layout.jsx
+++ views/frontend-app/app/layout.jsx
@@ -1,6 +1,6 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import LateralBar from "@/components/layout/LateralBar/LateralBar";
+import LateralBar from "../components/layout/LateralBar/LateralBar";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
