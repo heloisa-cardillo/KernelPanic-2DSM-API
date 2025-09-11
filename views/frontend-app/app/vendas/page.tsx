c296995b5ab9f69877a154dbd0af7329f9250095
Vinícius Leite <vinicius.leite17@fatec.sp.gov.br>
2025-09-11 16:26:08 -0300

    Feature (Pagina com ilustração de backend)
    A pagina tem ilustração de como ficaria com a ajuda do backend da aplicação.

--- views/frontend-app/app/vendas/page.tsx
+++ views/frontend-app/app/vendas/page.tsx
@@ -1,33 +1,40 @@
-import React from 'react'
-import './vendedores.css'
+'use client';
 
-export default function page() {
-  const vendedores = () => {
-    const dadosVendedores = [
-      {nome:"Paula",}
-    ]
-  }
+import React from 'react';
+import './vendedores.css';
+
+export default function Page() {
+  // apenas para ilustrar, como ainda nao tem integração com o backend
+  const vendedores = [
+    { nome: "Paula", visitas: 10, vendas: 5, taxa: "50%", faturamento: "R$ 1000" },
+    { nome: "Carlos", visitas: 8, vendas: 4, taxa: "50%", faturamento: "R$ 800" }
+  ];
+
   return (
     <div className='desempenho-vendedores'>
       <h1>Desempenho de vendedores</h1>
-        <table className='tabela-desempenho'>
-          <thead>
-            <tr>
-              <th>Vendedor</th>
-              <th>Visitas</th>
-              <th>Vendas</th>
-              <th>Taxa de conversão</th>
-              <th>Faturamento</th>
+      <table className='tabela-desempenho'>
+        <thead>
+          <tr>
+            <th>Vendedor</th>
+            <th>Visitas</th>
+            <th>Vendas</th>
+            <th>Taxa de conversão</th>
+            <th>Faturamento</th>
+          </tr>
+        </thead>
+        <tbody>
+          {vendedores.map((vendedor, index) => (
+            <tr key={index}>
+              <td>{vendedor.nome}</td>
+              <td>{vendedor.visitas}</td>
+              <td>{vendedor.vendas}</td>
+              <td>{vendedor.taxa}</td>
+              <td>{vendedor.faturamento}</td>
             </tr>
-            <tr>
-              <td>Vendedor1</td>
-              <td>X</td>
-              <td>X</td>
-              <td>X%</td>
-              <td>X</td>
-            </tr>
-          </thead>
-        </table>
+          ))}
+        </tbody>
+      </table>
     </div>
-  )
+  );
 }
