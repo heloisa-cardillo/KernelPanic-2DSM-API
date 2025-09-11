7c9113dfa11c647c07b4d466d2086871744c61a9
Vinícius Leite <vinicius.leite17@fatec.sp.gov.br>
2025-09-11 14:42:49 -0300

    Feature (Criando a pagina desempenho)
    Criação da pagina desempenho

--- views/frontend-app/app/vendas/page.tsx
+++ views/frontend-app/app/vendas/page.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
+import './vendedores.css'
 
 export default function page() {
+  const vendedores = () => {
+    const dadosVendedores = [
+      {nome:"Paula",}
+    ]
+  }
   return (
-    <div>
-        <h1>Vendas page</h1>
+    <div className='desempenho-vendedores'>
+      <h1>Desempenho de vendedores</h1>
+        <table className='tabela-desempenho'>
+          <thead>
+            <tr>
+              <th>Vendedor</th>
+              <th>Visitas</th>
+              <th>Vendas</th>
+              <th>Taxa de conversão</th>
+              <th>Faturamento</th>
+            </tr>
+            <tr>
+              <td>Vendedor1</td>
+              <td>X</td>
+              <td>X</td>
+              <td>X%</td>
+              <td>X</td>
+            </tr>
+          </thead>
+        </table>
     </div>
   )
 }
