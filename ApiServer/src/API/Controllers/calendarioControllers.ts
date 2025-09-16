511d5e082e7a2fa54ad3967925368d4a11974628
VitorSerpa <vitorserpa123@gmail.com>
2025-09-16 15:49:16 -0300

    feat(Controller e Routes acertado)

---
+++ ApiServer/src/API/Controllers/calendarioControllers.ts
@@ -0,0 +1,10 @@
+
+import { Request, Response } from "express"
+
+export const getLembrete = (req: Request, res: Response) => {
+    res.json({ message: "deu bom" })
+}
+
+export const postLembrete = (req: Request, res: Response) => {
+    res.json({ message: "deu bom" })
+}
--- ApiServer/src/API/Routes/calendarioRoutes.ts
+++ ApiServer/src/API/Routes/calendarioRoutes.ts
@@ -1,22 +1,11 @@
 import express from "express"
+import { getLembrete, postLembrete } from "../Controllers/calendarioControllers"
+
 const router = express.Router()
 
-router.get("/", (req, res)=>{
-    res.send("Ta funfando")
-})
+router.get("/", getLembrete)
 
-router.post("/", (req, res)=>{
-    const body = req.body
-    res.send(body)
-})
-
-router.put("/", (req, res)=>{
-    res.send("Ta funfando")
-})
-
-router.delete("/", (req, res)=>{
-    res.send("Ta funfando")
-})
+router.post("/", postLembrete)
 
 
 module.exports = router
