06cdfa7d0fcfc582f1d9e7c74bb868a769f3ea86
danprsp <daniel.piloto@fatec.sp.gob.br>
2025-09-23 21:43:39 -0300

    feat /rotas cliente com funcionalidade de cadastro de clietes

--- ApiServer/src/DAL/ormconfig.ts
+++ ApiServer/src/DAL/ormconfig.ts
@@ -1,25 +1,13 @@
 import { DataSource } from "typeorm";
-import { Funcionario } from "./Models/Funcionario";
-import { Cliente } from "./Models/Cliente";
-import { AgendamentoInteracao } from "./Models/AgendamentoInteracao";
-import { ContatoCliente } from "./Models/ContatoCliente";
-import { EventoTreinamento } from "./Models/EventoTreinamento";
-import { FuncionariosConvidados } from "./Models/FuncionariosConvidados";
-import { FunilVendas } from "./Models/FunilVendas";
-import { HistoricoFunil } from "./Models/HistoricoFunil";
-import { InteracaoCliente } from "./Models/InteracaoCliente";
-import { Notificacao } from "./Models/Notificacao";
-import { NotificacaoConvidados } from "./Models/NotificacaoConvidados";
-import { Presenca } from "./Models/Presenca";
-import { Vendas } from "./Models/Vendas";
-// import dotenv from "dotenv";
+import { join } from "path";
 
-// dotenv.config();
+import dotenv from "dotenv";
+dotenv.config();
 
-const host = process.env.DB_HOST;
-const password = process.env.DB_PASSWORD;
-const username = process.env.DB_USERNAME;
-const database = process.env.DB_DB;
+const host = process.env.DB_HOST || "localhost";
+const password = process.env.DB_PASSWORD || "fatec";
+const username = process.env.DB_USERNAME || "root";
+const database = process.env.DB_DB || "newe_database";
 
 export const AppDataSource = new DataSource({
   type: "mysql",
@@ -29,23 +17,26 @@ export const AppDataSource = new DataSource({
   password,
   database,
   // Use os arquivos compilados JS para evitar problemas de importação circular
+  // Caminho dinâmico para os arquivos .ts ou .js
   entities: [
-    AgendamentoInteracao, Cliente, ContatoCliente, 
-    EventoTreinamento, Funcionario, FuncionariosConvidados, 
-    FunilVendas, HistoricoFunil, InteracaoCliente,
-    Notificacao, NotificacaoConvidados, Presenca,
-    Vendas],
-  migrations: ["dist/DAL/Migrations/*.ts"],
+    join(__dirname, "./Models/*.{ts,js}")
+  ],
+  migrations: [
+    join(__dirname, "./Migrations/*.{ts,js}")
+  ],
   synchronize: false,
   logging: true,
 });
 
 
 // comando para gerar nova migration (apontando para o arquivo TypeScript)
-//// npx typeorm migration:generate -p src/DAL/migrations/PrimeiraMigration -d src/DAL/ormconfig.ts
+//// npx typeorm migration:generate src/DAL/Migrations/PrimeiraMigration -d dist/DAL/ormconfig.js
 
 // compilar o TypeScript para JavaScript
 //// npx tsc
 
 // depois, rodar a migration com o arquivo JavaScript compilado
 //// npx typeorm migration:run -d dist/DAL/ormconfig.js
+
+////// orm debug
+/// npx typeorm schema:log -d dist/DAL/ormconfig.js
