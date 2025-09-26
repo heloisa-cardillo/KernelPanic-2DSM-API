74628f3c366e5d6d1f2d634f820a1c2bd3251748
Templasan <joaovictordosreissantos439@gmail.com>
2025-09-26 17:20:34 -0300

    fi(Merge)

--- ApiServer/src/API/Routes/eventoEmailRoutes.ts
+++ ApiServer/src/API/Routes/eventoEmailRoutes.ts
@@ -3,13 +3,8 @@ import { postEvento , getEventos} from "../Controllers/eventoEmailController";
 
 const router = Router();
 
-<<<<<<< HEAD
-// ===== Rota POST para criação de evento =====
-router.post("/", postEvento);
-=======
 router.post("/lembrete", postEvento);
 
 router.get("/", getEventos)
->>>>>>> 0200f270d4f9cdbf3bcfecfea1ab414d9b3ebfe6
 
 module.exports = router;
--- ApiServer/src/main.ts
+++ ApiServer/src/main.ts
@@ -1,22 +1,12 @@
 import "reflect-metadata"; // Necessário para funcionar com decorators do TypeORM
 import app from "./app";    // Importa a instância do Express configurada
 
-const PORT = process.env.PORT || 5000; // Define a porta do servidor (env ou 5000)
+const PORT = process.env.PORT || 5050; // Define a porta do servidor (env ou 5000)
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
   // Mensagem que o servidor está rodando, mostrando a URL
 });
 
-<<<<<<< HEAD
 // // Chamada da função para executar o teste
 // testarClienteService();
-=======
-app.listen(PORT, ()=>{
-  console.log("Servidor rodando em http://localhost:5000")
-})
-
-
-
-
->>>>>>> 0200f270d4f9cdbf3bcfecfea1ab414d9b3ebfe6
