87db7afe67908e6dafc1a39633cbb3dc13de5745
miguelnonaka <miguelnonaka@gmail.com>
2025-09-27 01:16:17 -0300

    Update app.ts

--- ApiServer/src/app.ts
+++ ApiServer/src/app.ts
@@ -1,12 +1,11 @@
-import "reflect-metadata"; // Necessário para decorators funcionarem no TypeORM e outras libs
-import express from "express"; // Framework web para Node.js
-import cors from "cors"; // Middleware para habilitar CORS (compartilhamento de recursos entre origens)
-import dotenv from "dotenv"; // Carrega variáveis de ambiente do arquivo .env
+import "reflect-metadata"; // necessário para TypeORM
+import express from "express"
+import cors from "cors"
 
-import { iniciarCron } from "./API/Jobs/emailCron"; // Importa função para iniciar tarefas agendadas
-const eventoRoute = require("./API/Routes/eventoEmailRoutes"); // Importa rotas para eventos de email
+const app = express();
 
-dotenv.config(); // Carrega configurações do .env para process.env
+app.use(cors())
+app.use(express.json())
 
 const calendarioRoute = require("./API/Routes/calendarioRoutes"); // Rotas do calendário
 const funilVendasRoute = require("./API/Routes/funilVendasRoutes"); // Rotas do funil de vendas
@@ -16,37 +15,27 @@ const historicoRoute = require("./API/Routes/historicoRoutes")
 const pegarVendedor = require("./API/Routes/VendedorRoutes")
 
 
-const app = express(); // Cria instância do Express
+//Conexão com o banco de dados
+import { AppDataSource } from "./DAL/ormconfig";
 
-app.use(cors()); // Ativa CORS para todas as rotas
-app.use(express.json()); // Middleware para interpretar JSON nas requisições
-
-import { AppDataSource } from "./DAL/ormconfig"; // Configuração do TypeORM / banco
-
-// Inicializa conexão com banco de dados
 AppDataSource.initialize()
   .then(() => {
     console.log("✅ Conectado ao MySQL com sucesso!");
   })
-  .catch((err: Error) => {
-    // Caso falhe conexão, mostra erro e alerta sobre .env
+  .catch((err:Error) => {
     console.error(err, "❌ Erro ao conectar ao MySQL, VERIFIQUE SE O .ENV ESTA CONFIGURADO CORRETAMENTE!!!:");
   });
 
-// Define as rotas da aplicação
-app.use("/eventos", eventoRoute); // Rota para eventos relacionados a email
-iniciarCron(); // Inicia tarefas agendadas (jobs)
-app.use("/funilVendas", funilVendasRoute); // Rotas para funil de vendas
-app.use("/calendario", calendarioRoute); // Rotas do calendário
-app.use("/clientes", cadastroCliente); // Rotas para clientes
-
+app.use("/funilVendas",funilVendasRoute )
+app.use("/calendario", calendarioRoute)
+app.use("/clientes", cadastroCliente)
 app.use("/vendedor",pegarVendedor)
 app.use("/historico",historicoRoute)
 app.use("/gestao",gestaoRoute)
 
-// Rota raiz para teste rápido da API
-app.get("/", (req, res) => {
-  res.send("API funcionando");
-});
 
-export default app; // Exporta o app para ser usado em outro lugar (ex: server.ts)
+app.get("/", (req, res) => {
+    res.send("API funcionando")
+})
+
+export default app
